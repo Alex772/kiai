54933df31121b39c651f1e2f32dbb9f753551d9a
Alex772 <alexsandrosantos.9999@gmail.com>
2026-07-13 07:36:34 -0300

    Cargo temporario, comandos de beneficios, ajustes de cartao

--- src/registerCommands.ts
+++
@@ -1,7 +0,0 @@
-import { assertRequiredEnv } from './config.js';
-import { registerSlashCommands } from './registerSlashCommands.js';
-
-assertRequiredEnv();
-
-const result = await registerSlashCommands();
-console.log(`Registrados ${result.count} comandos slash no escopo ${result.scope}.`);
